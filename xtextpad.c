2e27f154d01f2d360186a135d5bd3e4b4761abc9
jonghyouk yun <ageldama@gmail.com>
2026-09-08 00:42:04 +0900

    --wip-- [skip ci]

--- xtextpad.c
+++ xtextpad.c
@@ -6,113 +6,44 @@
 #include <X11/keysym.h>
 #include <X11/Xatom.h>
 
+
 void send_shift_insert(Display *display, Window target_window)
 {
-    XKeyEvent event;
-    KeyCode insert_code, shift_code;
-    
-    // Get keycodes
-    insert_code = XKeysymToKeycode(display, XK_Insert);
-    shift_code = XKeysymToKeycode(display, XK_Shift_L);
-    
-    // Create KeyPress event for Insert
-    event.type = KeyPress;
-    event.display = display;
-    event.window = target_window;
-    event.root = DefaultRootWindow(display);
-    event.subwindow = None;
-    event.time = CurrentTime;
-    event.x = 1;
-    event.y = 1;
-    event.x_root = 1;
-    event.y_root = 1;
-    event.same_screen = True;
-    event.state = ShiftMask;  // Shift modifier already held
-    event.keycode = insert_code;
+  XKeyEvent event;
+  KeyCode insert_code; //, shift_code;
 
-    #if 0
-      // 3. Clear modifiers (--clearmodifiers equivalent)
-  // Send Shift Press
+  insert_code = XKeysymToKeycode(display, XK_Insert);
+  //shift_code = XKeysymToKeycode(display, XK_Shift_L);
+
+  event.type = KeyPress;
+  event.display = display;
+  event.window = target_window;
+  event.root = DefaultRootWindow(display);
+  event.subwindow = None;
+  event.time = CurrentTime;
+  event.x = 1;
+  event.y = 1;
+  event.x_root = 1;
+  event.y_root = 1;
+  event.same_screen = True;
+  event.state = ShiftMask;
+  event.keycode = insert_code;
+
+#if 0
+  // 3. Clear modifiers (--clearmodifiers equivalent)
   event.type = KeyPress;
   event.keycode = shift_code;
   event.state = 0; // No modifiers active initially
   XSendEvent(display, target_window, True, KeyPressMask, (XEvent *)&event);
 #endif
-    
-    // Send synthetic event
-    XSendEvent(display, target_window, True, KeyPressMask, (XEvent *)&event);
-    
-    // Also send KeyRelease
-    event.type = KeyRelease;
-    XSendEvent(display, target_window, True, KeyReleaseMask, (XEvent *)&event);
-    
-    XFlush(display);
-}
 
-#if 0
-void send_shift_insert_to_window(GdkDisplay *gdk_display, Window target_wid) {
-  if (!GDK_IS_X11_DISPLAY(gdk_display)) {
-    g_printerr("Error: Window event injection requires an X11 display.\n");
-    exit(EXIT_FAILURE);
-  }
+  XSendEvent(display, target_window, True, KeyPressMask, (XEvent *)&event);
 
-  Display *xdisplay = GDK_DISPLAY_XDISPLAY(gdk_display);
-
-  // 1. Resolve keysyms to keycodes
-  KeyCode shift_code = XKeysymToKeycode(xdisplay, XK_Shift_L);
-  KeyCode insert_code = XKeysymToKeycode(xdisplay, XK_Insert);
-
-  if (shift_code == 0 || insert_code == 0) {
-    g_printerr("Failed to resolve keycodes.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  // 2. Prepare XEvent structure targeting $WID
-  XEvent event;
-  memset(&event, 0, sizeof(event));
-  event.xkey.display     = xdisplay;
-  event.xkey.window      = target_wid;
-  event.xkey.root        = DefaultRootWindow(xdisplay);
-  event.xkey.subwindow   = None;
-  event.xkey.time        = CurrentTime;
-  event.xkey.x           = 1;
-  event.xkey.y           = 1;
-  event.xkey.x_root      = 1;
-  event.xkey.y_root      = 1;
-  event.xkey.same_screen = True;
-
-  // 3. Clear modifiers (--clearmodifiers equivalent)
-  // Send Shift Press
-  event.type = KeyPress;
-  event.xkey.keycode = shift_code;
-  event.xkey.state = 0; // No modifiers active initially
-  XSendEvent(xdisplay, target_wid, True, KeyPressMask, &event);
-
-  // Send Insert Press with Shift state active
-  event.type = KeyPress;
-  event.xkey.keycode = insert_code;
-  event.xkey.state = ShiftMask; // Apply Shift modifier state
-  XSendEvent(xdisplay, target_wid, True, KeyPressMask, &event);
-
-  // Send Insert Release
   event.type = KeyRelease;
-  event.xkey.keycode = insert_code;
-  event.xkey.state = ShiftMask;
-  XSendEvent(xdisplay, target_wid, True, KeyReleaseMask, &event);
+  XSendEvent(display, target_window, True, KeyReleaseMask, (XEvent *)&event);
 
-  // Send Shift Release
-  event.type = KeyRelease;
-  event.xkey.keycode = shift_code;
-  event.xkey.state = 0;
-  XSendEvent(xdisplay, target_wid, True, KeyReleaseMask, &event);
-
-  // Flush event queue to transmit immediately
-  XFlush(xdisplay);
+  XFlush(display);
 }
-#endif
-
-
-
 
 
 // Returns the active Window ID on X11 display
@@ -156,93 +87,22 @@ Window get_active_window_gtk(GdkDisplay *gdk_display) {
 }
 
 
-
-// X11 클립보드 데이터를 유지하기 위한 백그라운드 데몬 프로세스
-static void daemonize_and_hold_clipboard(Display *display, const char *text) {
-    pid_t pid = fork();
-    if (pid < 0) return;
-    if (pid > 0) return; // 부모 프로세스는 바로 종료
-
-    // 자식 프로세스: 세션 분리
-    setsid();
-
-    Window root = DefaultRootWindow(display);
-    Window owner = XCreateSimpleWindow(display, root, 0, 0, 1, 1, 0, 0, 0);
-
-    Atom clipboard = XInternAtom(display, "CLIPBOARD", False);
-    Atom primary   = XInternAtom(display, "PRIMARY", False);
-    Atom utf8_str  = XInternAtom(display, "UTF8_STRING", False);
-    Atom targets   = XInternAtom(display, "TARGETS", False);
-
-    // PRIMARY 및 CLIPBOARD 소유권 획득
-    XSetSelectionOwner(display, clipboard, owner, CurrentTime);
-    XSetSelectionOwner(display, primary, owner, CurrentTime);
-
-    int held_selections = 2;
-    XEvent event;
-
-    // 다른 앱이 클립보드 소유권을 가져갈 때까지 요청 응답 서비스 제공
-    while (held_selections > 0) {
-        XNextEvent(display, &event);
-
-        if (event.type == SelectionRequest) {
-            XSelectionRequestEvent *req = &event.xselectionrequest;
-            XSelectionEvent sel_evt = {
-                .type = SelectionNotify,
-                .requestor = req->requestor,
-                .selection = req->selection,
-                .target = req->target,
-                .property = req->property,
-                .time = req->time
-            };
-
-            if (req->target == targets) {
-                Atom supp[] = { targets, utf8_str, XA_STRING };
-                XChangeProperty(display, req->requestor, req->property, XA_ATOM, 32,
-                                PropModeReplace, (unsigned char *)supp, 3);
-            } else if (req->target == utf8_str || req->target == XA_STRING) {
-                XChangeProperty(display, req->requestor, req->property, req->target, 8,
-                                PropModeReplace, (unsigned char *)text, strlen(text));
-            } else {
-                sel_evt.property = None;
-            }
-
-            XSendEvent(display, req->requestor, True, 0, (XEvent *)&sel_evt);
-            XFlush(display);
-        } 
-        else if (event.type == SelectionClear) {
-            held_selections--;
-        }
-    }
-
-    printf("TEXT:[%s]\n", text);
-
-    XDestroyWindow(display, owner);
-    XCloseDisplay(display);
-    exit(EXIT_SUCCESS);
-}
-
-
 static void copy_buffer_to_clipboard(GtkTextBuffer *buffer) {
-    /* GtkClipboard *clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD); */
-    /* GtkClipboard *primary   = gtk_clipboard_get(GDK_SELECTION_PRIMARY); */
+  GtkClipboard *clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
+  GtkClipboard *primary   = gtk_clipboard_get(GDK_SELECTION_PRIMARY);
 
-    GtkTextIter start, end;
-    gtk_text_buffer_get_bounds(buffer, &start, &end);
-    gchar *text = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
+  GtkTextIter start, end;
+  gtk_text_buffer_get_bounds(buffer, &start, &end);
+  gchar *text = gtk_text_buffer_get_text(buffer, &start, &end, FALSE);
 
-    GdkDisplay *gdk_disp = gdk_display_get_default();
-    Display *xdisplay = GDK_DISPLAY_XDISPLAY(gdk_disp);
-    daemonize_and_hold_clipboard(xdisplay, text);
+  gtk_clipboard_set_text(clipboard, text, -1);
+  gtk_clipboard_set_text(primary, text, -1);
 
-    /* gtk_clipboard_set_text(clipboard, text, -1); */
-    /* gtk_clipboard_set_text(primary, text, -1); */
-    /*  */
-    /* gtk_clipboard_store(clipboard); */
+  gtk_clipboard_store(clipboard);
 
-    /* g_print("--- COPIED ---\n%s\n----------------------\n", text); */
+  /* g_print("--- COPIED ---\n%s\n----------------------\n", text); */
 
-    g_free(text);
+  g_free(text);
 
 }
 
@@ -254,6 +114,13 @@ typedef struct {
   Window target_window;
 } AppWidgets;
 
+
+static gboolean delayed_exit_callback(gpointer user_data) {
+    AppWidgets *app = (AppWidgets *)user_data;
+    g_application_quit(G_APPLICATION(app->app));
+    return G_SOURCE_CONTINUE; // G_SOURCE_REMOVE?
+}
+
 static void on_button_clicked(GtkButton *button, gpointer user_data) {
   AppWidgets *widgets = (AppWidgets *)user_data;
 
@@ -263,7 +130,8 @@ static void on_button_clicked(GtkButton *button, gpointer user_data) {
   Display *xdisplay = GDK_DISPLAY_XDISPLAY(display);
   send_shift_insert(xdisplay, widgets->target_window);
 
-  g_application_quit(G_APPLICATION(widgets->app));
+  //g_application_quit(G_APPLICATION(widgets->app));
+  g_timeout_add(50, delayed_exit_callback, user_data);
 }
 
 static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
